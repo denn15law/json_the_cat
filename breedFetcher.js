b4263e46b9cf8b3681d885b3b16db952e11e76fb
Dennis Law <denn15_law@live.ca>
2021-11-04 19:06:49 +0000

    handle edge cases for breed not found and request failed

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,8 +1,26 @@
 const request = require("request");
+const args = process.argv.slice(2);
 
 request(
-  "https://api.thecatapi.com/v1/breeds/search?q=siberian",
+  `https://api.thecatapi.com/v1/breeds/search?q=${args}`,
   (err, res, body) => {
-    console.log(body);
+    //edge case: if there is an error with request (broken url)
+    if (err) {
+      console.log(`Error: Request failed\nError code: ${err.code}`);
+      return;
+    }
+
+    //Use JSON.parse to obtain string into object
+    const data = JSON.parse(body);
+
+    //edge case: if there is an error (no breeds found)
+    if (data.length === 0) {
+      console.log("Error: No Breeds found");
+      return;
+    }
+
+    //Return description for search results
+    console.log("\n------Description------\n");
+    console.log(data[0].description);
   }
 );
